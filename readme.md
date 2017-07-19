a4122a793ad4b1d311f6132c14c7c5ba00c3ad49
Theophile Kalumbu <theophile@ouicar.fr>
2017-07-20 00:25:33 +0200

    feat(docs): add documentation

---
+++ readme.md
@@ -0,0 +1,3 @@
+# ELM Simple Storybook
+
+A simple storybook for ELM inspired by React-Storybook
--- src/StoryBook/Model.elm
+++ src/StoryBook/Model.elm
@@ -1,8 +1,24 @@
 module StoryBook.Model exposing (..)
 
+{-|
+
+Storybook datastructures
+
+# Story
+@docs Story
+
+# Stories
+@docs Stories
+
+# Model
+@docs Model
+-}
+
 import Html exposing (Html)
 
 
+{-| Item used to describe a Story
+-}
 type alias Story msg =
     { id : String
     , description : String
@@ -10,10 +26,14 @@ type alias Story msg =
     }
 
 
+{-| Item used to describe a List of stories
+-}
 type alias Stories msg =
     List (Story msg)
 
 
+{-| Model of the storybook
+-}
 type alias Model msg =
     { stories : Stories msg
     , selectedStoryId : Maybe String
