b58e1b420fbeb9ee05add4cff4aebc3a8851303f
Theophile Kalumbu <theophile@ouicar.fr>
2017-09-07 21:52:13 +0200

    feat(migrate): migrate to bulimia css

--- examples/design-system/src/Atom/Global/Colors/Index.elm
+++ examples/design-system/src/Atom/Global/Colors/Index.elm
@@ -1,23 +1,22 @@
 module Atom.Global.Colors.Index exposing (..)
 
 import Html exposing (Html, text, div)
-import Elegant exposing (..)
-import Color exposing (..)
+import Html.Attributes exposing (style)
 
 
 type alias Model =
-    { colors : List Color }
+    { colors : List String }
 
 
-colorBox : Color -> Html msg
+colorBox : String -> Html msg
 colorBox color =
     div
         [ style
-            [ backgroundColor color
-            , width (Px 80)
-            , height (Px 80)
-            , margin (Px 10)
-            , marginLeft (Em 0)
+            [ ( "backgroundColor", color )
+            , ( "width", "80px" )
+            , ( "height", "80px" )
+            , ( "margin", "10px" )
+            , ( "marginLeft", "0px" )
             ]
         ]
         []
@@ -25,7 +24,7 @@ colorBox color =
 
 view : Model -> Html msg
 view m =
-    div [ style [ displayFlex ] ]
+    div [ style [ ( "display", "flex" ) ] ]
         (List.map
             (\c -> colorBox c)
             m.colors
