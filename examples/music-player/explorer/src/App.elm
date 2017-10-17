30f815508a0a109d31992f896aeb0d05dcb1e1f1
Theophile Kalumbu <theophile@ouicar.fr>
2017-10-17 13:31:03 +0200

    fix(music-player): fromList util

--- examples/music-player/explorer/src/App.elm
+++ examples/music-player/explorer/src/App.elm
@@ -1,6 +1,6 @@
 module App exposing (..)
 
-import UIExplorer exposing (app, addUICategory, emptyUICategories, createUI)
+import UIExplorer exposing (app, fromUIList, createUI)
 import Stories.PlayPause as PlayPause
 import Stories.Controls as Controls
 import Stories.TrackList as TrackList
@@ -8,17 +8,15 @@ import Stories.TrackList as TrackList
 
 main =
     app
-        (emptyUICategories
-            |> addUICategory
-                "Default"
-                [ createUI
-                    "PlayPause"
-                    PlayPause.viewStories
-                , createUI
-                    "Controls"
-                    Controls.viewStories
-                , createUI
-                    "TrackList"
-                    TrackList.viewStories
-                ]
+        (fromUIList
+            [ createUI
+                "PlayPause"
+                PlayPause.viewStories
+            , createUI
+                "Controls"
+                Controls.viewStories
+            , createUI
+                "TrackList"
+                TrackList.viewStories
+            ]
         )
