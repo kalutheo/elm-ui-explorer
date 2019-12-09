56287d6527c652454b8fb30813dfa2dfd4f48bf4
kalutheo <kalutheo@hotmail.com>
2019-12-09 23:05:00 +0100

    refactor(content): move to raw

--- examples/button/ExplorerWithNotes.elm
+++ examples/button/ExplorerWithNotes.elm
@@ -1,7 +1,10 @@
 module ExplorerWithNotes exposing (main)
 
 import Button exposing (..)
-import Html
+import Html exposing (Html, hr)
+import Html.Attributes as Attr
+import NoUnused.Variables
+import RawContent
 import UIExplorer
     exposing
         ( UIExplorerProgram
@@ -17,105 +20,13 @@ type alias PluginOption =
     }
 
 
-note =
-    { note = """
-# Modules
-- [Button](#button)
-
-# Button
-- [Config](#config)
-- [Size](#size)
-- [Kind](#kind)
-- [Appearance](#appearance)
-- [defaultButtonConfig](#defaultbuttonconfig)
-- [view](#view)
-
-## Button
-The Button should be used to trigger user actions.
-Some examples of interactions:
-  - Submit a form
-  - Cancel an order
-  - Toggle a menu visibility
-  - Play a media
-```elm
-import Button exposing (..)
-Button.view "Submit" defaultButtonConfig ()
-```
-## Links:
-  - [UX Planet - Basic rules for button](https://uxplanet.org/7-basic-rules-for-button-design-63dcdf5676b4)
-
-### `Config`
-```elm
-type alias Config  =
-    { appearance : Button.Appearance, size : Button.Size, kind : Button.Kind, class : String.String, theme : Button.Theme }
-```
- Option to customize the Button
-
-
----
-
-
-### `Size`
-```elm
-type Size
-    = S
-    | M
-    | L
-```
- Define the size of the Button
-
-
----
-
-
-### `Kind`
-```elm
-type Kind
-    = Link
-    | Filled
-    | Ghost
-```
- Look and feel of the Button
-
-
----
-
-
-### `Appearance`
-```elm
-type Appearance
-    = Primary
-    | Secondary
-```
- Define the appearance of the Button
-
-
----
-
-
-### `defaultButtonConfig`
-```elm
-defaultButtonConfig : Config
-```
- Default Configurations
-
-
----
-
-
-### `view`
-```elm
-view : String.String -> Config -> msg -> Html.Html msg
-```
- Renders the button
-
-
----
-
-
-> Generated with elm: 0.19.0 and elm-docs: 0.4.0
-
-""" }
+options =
+    { note = RawContent.note
+    , review =
+        { errors = ReviewPlugin.initErrors [ NoUnused.Variables.rule ] RawContent.sourceCode
+        , sourceCode = RawContent.sourceCode
+        }
+    }
 
 
 main : UIExplorerProgram {} () PluginOption
@@ -124,12 +35,12 @@ main =
         { defaultConfig | viewEnhancer = Note.viewEnhancer }
         [ storiesOf
             "Button"
-            [ ( "Primary", \_ -> Button.view "Submit" defaultButtonConfig (), note )
-            , ( "Secondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary } (), note )
-            , ( "Small", \_ -> Button.view "Submit" { defaultButtonConfig | size = S } (), note )
-            , ( "Large", \_ -> Button.view "Submit" { defaultButtonConfig | size = L } (), note )
-            , ( "Link", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Link, appearance = Secondary } (), note )
-            , ( "Ghost Primary", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Ghost } (), note )
-            , ( "GhostSecondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary, kind = Ghost } (), note )
+            [ ( "Primary", \_ -> Button.view "Submit" defaultButtonConfig (), options )
+            , ( "Secondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary } (), options )
+            , ( "Small", \_ -> Button.view "Submit" { defaultButtonConfig | size = S } (), options )
+            , ( "Large", \_ -> Button.view "Submit" { defaultButtonConfig | size = L } (), options )
+            , ( "Link", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Link, appearance = Secondary } (), options )
+            , ( "Ghost Primary", \_ -> Button.view "Submit" { defaultButtonConfig | kind = Ghost } (), options )
+            , ( "GhostSecondary", \_ -> Button.view "Submit" { defaultButtonConfig | appearance = Secondary, kind = Ghost } (), options )
             ]
         ]
