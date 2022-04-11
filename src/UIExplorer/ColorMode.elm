4d93469614847cf8d478b387af8411ca17a35169
Théophile KALUMBU <kalutheo@hotmail.com>
2022-04-11 23:02:46 +0200

    chore(bump): bump
    bump

--- src/UIExplorer/ColorMode.elm
+++ src/UIExplorer/ColorMode.elm
@@ -1,11 +1,20 @@
 module UIExplorer.ColorMode exposing (ColorMode(..), colorModeToString)
 
 
+{-|
+@docs ColorMode
+@docs colorModeToString
+--}
+
+{-| This is used to determine the color mode of the UI Explorer.
+-}
 type ColorMode
     = Dark
     | Light
 
 
+{-| Convert a colorMode to a string.
+-}
 colorModeToString : ColorMode -> String
 colorModeToString colorMode =
     case colorMode of
